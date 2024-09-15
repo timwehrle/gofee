5ec24e765a6b352e93127eb6a15801ffdef2b552
timwehrle <wetigg@gmail.com>
2024-09-15 22:49:44 +0200

    tests: add entropy calculation tests

--- pkg/gofee/entropy.go
+++ pkg/gofee/entropy.go
@@ -1,7 +1,21 @@
 package gofee
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
-func CalculateEntropy(charsetSize, passwordLength int) float64 {
-	return float64(passwordLength) * math.Log2(float64(charsetSize))
+// CalculateEntropy returns the entropy (in bits) of a password,
+// given the character set size and password length.
+func CalculateEntropy(charsetSize, passwordLength int) (float64, error) {
+	// Return an error if the charset size or password length is invalid.
+	if charsetSize <= 0 {
+		return 0, fmt.Errorf("charset size must be greater than 0")
+	}
+	if passwordLength <= 0 {
+		return 0, fmt.Errorf("password length must be greater than 0")
+	}
+
+	// Calculate entropy using the formula: entropy = passwordLength * log2(charsetSize)
+	return float64(passwordLength) * math.Log2(float64(charsetSize)), nil
 }
